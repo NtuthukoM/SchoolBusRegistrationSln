cf857d5754f09169656f763f82f1beb5a0af8944
NtuthukoM <ntuthukorocks@gmail.com>
2023-10-23 20:35:18 +0200

    Adds web app and db context

--- SchoolBusRegistration.Domain/SchoolBusRegistrationContext.cs
+++ SchoolBusRegistration.Domain/SchoolBusRegistrationContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace SchoolBusRegistration.Domain
 {
-    public class SchoolBusRegistrationContext:
+    public class SchoolBusRegistrationContext:DbContext
     {
+        public DbSet<TransportRequest> TransportRequests { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Parent> Parents { get; set; }
     }
 }
